77d0e326dda680afa4dcd322ed08e3c24c536367
Aidan Kaczanowski <akaczano@purdue.edu>
2020-02-11 17:57:48 -0500

    Added landmarks to improve accuracy; improved map scaling; added utilities for polygon visualization, file choosing, etc.

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -5,6 +6,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -20,23 +22,62 @@ public class GraphicsPanel extends JPanel implements MouseListener, MouseMotionL
     private ArrayList<Integer> xs;
     private ArrayList<Integer> ys;
 
+    private ArrayList<Integer> cxs;
+    private ArrayList<Integer> cys;
+
     private int addedPoints = 0;
 
-    int screen_width = Window.WIDTH - 2 * MARGIN_X;
-    int screen_height = Window.HEIGHT - 2 * MARGIN_Y;
+    private int screen_width;
+    private int screen_height;
 
-    double long_per_x = screen_width / (MAX_LONG - MIN_LONG);
-    double lat_per_y = screen_height /  (MAX_LAT - MIN_LAT);
+    private int map_width;
+    private int map_height;
+
+    private double ratio;
+
+    private JLabel posLabel;
+
+    private boolean connect = false;
+
+    public void drawPolygon(){
+        connect = true;
+        this.repaint();
+    }
+
+    public void addLandmarks(ArrayList<Point> points) {
+        for (Point p : points) {
+
+            if (!isPointOnScreen(p)) {
+                continue;
+            }
+
+            int x = longToX(p.getLongitude());
+            int y = latToY(p.getLatitude());
+            cxs.add(x);
+            cys.add(y);
+        }
+
+    }
+
+    public void clearScreen() {
+
+        while (xs.size() > addedPoints) {
+            xs.remove(addedPoints);
+            ys.remove(addedPoints);
+        }
+        this.repaint();
+    }
+
 
     public void addPoints(ArrayList<Point> points) {
 
-        addedPoints += points.size();
-
         for (Point p : points) {
-
-            int x = (int)((p.getLongitude()-MIN_LONG) * long_per_x);
-            int y = (int)((p.getLatitude()-MIN_LAT) * lat_per_y);
-            y = screen_height - y;
+            if (!isPointOnScreen(p)) {
+                continue;
+            }
+            addedPoints ++;
+            int x = longToX(p.getLongitude());
+            int y = latToY(p.getLatitude());
             xs.add(x);
             ys.add(y);
         }
@@ -45,15 +86,84 @@ public class GraphicsPanel extends JPanel implements MouseListener, MouseMotionL
     public GraphicsPanel() {
         xs = new ArrayList<>();
         ys = new ArrayList<>();
+        cxs = new ArrayList<>();
+        cys = new ArrayList<>();
+
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
+        this.setLayout(new BorderLayout());
+
+        posLabel = new JLabel();
+        this.add(posLabel, BorderLayout.PAGE_END);
+    }
+
+    @Override
+    public void setBounds(int x, int y, int width, int height) {
+        super.setBounds(x, y, width, height);
+
+        screen_width = this.getBounds().width;
+        screen_height = this.getBounds().height;
+
+        double long_per_x = screen_width / (MAX_LONG - MIN_LONG);
+        double lat_per_y = screen_height /  (MAX_LAT - MIN_LAT);
+
+        ratio = Math.min(long_per_x, lat_per_y);
+        map_width = (int) (ratio * (MAX_LONG - MIN_LONG));
+        map_height = (int) (ratio * (MAX_LAT - MIN_LAT));
+    }
+
+    private double xToLong(int x) {
+        return (x - MARGIN_X) / ratio + MIN_LONG;
+    }
+
+    private double yToLat(int y) {
+        return ((map_height-y) - MARGIN_Y) / ratio + MIN_LAT;
+    }
+
+    private int longToX(double lon) {
+        return (int)((lon-MIN_LONG) * ratio);
+    }
+
+    private int latToY(double lat) {
+        int y = (int)((lat-MIN_LAT) * ratio);
+        y = map_height - y;
+        return y;
+    }
+
+    private boolean isPointOnScreen(Point p) {
+        if (p.getLongitude() >= MIN_LONG && p.getLongitude() <= MAX_LONG) {
+            if (p.getLatitude() >= MIN_LAT && p.getLatitude() <= MAX_LAT){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+        for (int j  = 0; j < cxs.size(); ++j) {
+            g.setColor(Color.BLUE);
+            g.fillOval(cxs.get(j), cys.get(j), 5, 5);
+        }
+        g.setColor(Color.BLACK);
+
         for (int i = 0; i < xs.size(); ++i) {
+            if (i == addedPoints) {
+                g.setColor(Color.BLUE);
+            }
             g.fillRect(xs.get(i), ys.get(i), 1, 1);
+
+            if (connect && i >= addedPoints) {
+
+                int startX = xs.get(i);
+                int endX = i+1 >= xs.size() ? xs.get(addedPoints) : xs.get(i+1);
+
+                int startY = ys.get(i);
+                int endY = i+1 >= ys.size() ? ys.get(addedPoints) : ys.get(i+1);
+
+                g.drawLine(startX, startY, endX, endY);
+            }
         }
     }
 
@@ -64,6 +174,10 @@ public class GraphicsPanel extends JPanel implements MouseListener, MouseMotionL
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
+        if (connect) {
+            connect = false;
+            clearScreen();
+        }
         xs.add(mouseEvent.getX());
         ys.add(mouseEvent.getY());
         this.repaint();
@@ -85,6 +199,11 @@ public class GraphicsPanel extends JPanel implements MouseListener, MouseMotionL
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
+        if (connect) {
+            connect = false;
+            clearScreen();
+        }
+
         xs.add(mouseEvent.getX());
         ys.add(mouseEvent.getY());
         this.repaint();
@@ -92,17 +211,19 @@ public class GraphicsPanel extends JPanel implements MouseListener, MouseMotionL
 
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
-
+        double longitude = ((mouseEvent.getX() - MARGIN_X) / ratio + MIN_LONG) * -1;
+        double latitude = ((map_height-mouseEvent.getY()) - MARGIN_Y) / ratio + MIN_LAT;
+        posLabel.setText(String.format("%.3f degrees west, %.3f degrees north", longitude, latitude));
     }
 
-    public void saveToCSV(String fileName) {
-        File file = new File(fileName);
+    public void saveToCSV(File file) {
+
         try {
             PrintWriter pw = new PrintWriter(file);
             pw.println("LONG,LAT");
             for (int i = addedPoints; i < xs.size(); ++i) {
-                double longitude = (xs.get(i) - MARGIN_X) / long_per_x + MIN_LONG;
-                double latitude = ((screen_height-ys.get(i)) - MARGIN_Y) / lat_per_y + MIN_LAT;
+                double longitude = xToLong(xs.get(i));
+                double latitude = yToLat(ys.get(i));
                 pw.println(String.format("%f,%f", longitude, latitude));
             }
             pw.close();
--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Window extends JFrame implements WindowListener {
+public class Window extends JFrame implements  ActionListener {
 
     public static int WIDTH = 600;
-    public static int HEIGHT = 500;
+    public static int HEIGHT = 410;
 
     private GraphicsPanel panel;
+
+    private JButton drawButton;
+    private JButton clearButton;
+    private JButton saveButton;
+
+
 
     public Window () {
         super("Polygon Builder");
@@ -18,20 +24,40 @@ public class Window extends JFrame implements WindowListener {
         setSize(new Dimension(WIDTH, HEIGHT));
         setResizable(false);
         this.setLayout(null);
-        this.addWindowListener(this);
         initComponents();
     }
 
     private void initComponents() {
         panel = new GraphicsPanel();
-        panel.setBounds(0, 0, WIDTH, HEIGHT);
+        panel.setBounds(0, 20, WIDTH, 300);
         try {
             panel.addPoints(Point.readCoordinates(new File("map.csv")));
+            panel.addPoints(Point.readCoordinates(new File("mexico.csv")));
+            panel.addLandmarks(Point.readCoordinates(new File("cities.csv")));
+            //panel.addPoints(Point.readCoordinates(new File("canada.csv")));
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         this.add(panel);
+
+        JPanel otherPanel = new JPanel();
+        otherPanel.setLayout(new FlowLayout());
+
+        drawButton = new JButton("Draw Polygon");
+        clearButton = new JButton("Clear");
+        saveButton = new JButton("Save");
+
+        drawButton.addActionListener(this);
+        clearButton.addActionListener(this);
+        saveButton.addActionListener(this);
+
+        otherPanel.add(drawButton);
+        otherPanel.add(clearButton);
+        otherPanel.add(saveButton);
+
+        otherPanel.setBounds(new Rectangle(0, 330, WIDTH, 200));
+        this.add(otherPanel);
     }
 
     public void launch () {
@@ -39,37 +65,20 @@ public class Window extends JFrame implements WindowListener {
     }
 
     @Override
-    public void windowOpened(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowClosing(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowClosed(WindowEvent windowEvent) {
-        panel.saveToCSV("output.csv");
-    }
-
-    @Override
-    public void windowIconified(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowActivated(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent windowEvent) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == clearButton) {
+            panel.clearScreen();
+        }
+        else if (actionEvent.getSource() == saveButton) {
+            JFileChooser saveDialog = new JFileChooser();
+            saveDialog.setDialogTitle("Choose an output file");
+            int result = saveDialog.showSaveDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                panel.saveToCSV(saveDialog.getSelectedFile());
+            }
+        }
+        else if (actionEvent.getSource() == drawButton) {
+            panel.drawPolygon();
+        }
     }
 }
